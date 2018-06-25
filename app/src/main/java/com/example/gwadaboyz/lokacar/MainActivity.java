b42f49d2b60a14e5739ec38e658dd4eb728a700d
will971 <modeste.william.s@gmail.com>
2018-06-25 16:15:28 +0200

    premier intent

--- app/src/main/java/com/example/gwadaboyz/lokacar/MainActivity.java
+++ app/src/main/java/com/example/gwadaboyz/lokacar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.gwadaboyz.lokacar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void connexion(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
 }
