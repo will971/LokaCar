722f000a8e35dc4cb564d4d48e950f9fc0dcf464
Unknown <modeste.william.s@gmail.com>
2018-06-25 13:58:34 +0200

    dzada

---
+++ app/src/main/java/com/example/gwadaboyz/lokacar/BO/Vehicule.java
@@ -0,0 +1,12 @@
+package com.example.gwadaboyz.lokacar.BO;
+
+public class Vehicule {
+
+    private int id;
+    private String marque;
+    private String imamatriculation;
+    private boolean statut;
+    private int prix;
+    private Agence agence;
+
+}
