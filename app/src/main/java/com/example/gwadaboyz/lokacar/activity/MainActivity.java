7948d51392fcc546d48dfd48e30de0bc89f06bcc
will971 <modeste.william.s@gmail.com>
2018-06-27 10:17:19 +0200

    ajout des users dans une agence

--- app/src/main/java/com/example/gwadaboyz/lokacar/activity/MainActivity.java
+++ app/src/main/java/com/example/gwadaboyz/lokacar/activity/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.gwadaboyz.lokacar.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.view.View;
@@ -19,7 +19,6 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -48,27 +47,37 @@ public class MainActivity extends AppCompatActivity {
         agence1.put("nom","St-Herblain");
         agence1.put("adresse","123 rue moile 44200 St-Herblain");
         agence1.put("siret","456882-5558-255-5");
-        agences.document().set(agence1);
+        agences.document("A1").set(agence1);
 
         Map<String,Object> agence2 = new ArrayMap<>();
         agence2.put("nom","Orvault");
         agence2.put("adresse","52 rue poil 44300 NANTES");
         agence2.put("siret","55-555656595-22626262-162");
-        agences.document().set(agence2);
+        agences.document("A2").set(agence2);
 
         Map<String,Object> agence3 = new ArrayMap<>();
         agence3.put("nom","Nantes");
         agence3.put("adresse","233 rue lomp 44000 NANTES");
         agence3.put("siret","5555-555-55-555225-2");
-        agences.document().set(agence3);
+        agences.document("A3").set(agence3);
 
         Map<String,Object> agence4 = new ArrayMap<>();
         agence4.put("nom","Guadeloupe");
         agence4.put("adresse","36 rue dela 97100 Guadeloupe");
         agence4.put("siret","654656-56566262-626262");
-        agences.document().set(agence4);
+        agences.document("A4").set(agence4);
     }
+    public void addUser(){
+        CollectionReference users = db.collection("Agence").document("A1").collection("user");
+        Map<String, Object> user= new ArrayMap<>();
+        user.put("nom", "Gerant1");
+        user.put("prenom", "test");
+        user.put("mot de passe", "test");
+        user.put("mail", "test@test.fr");
 
+users.document("Gérant").set(user);
+
+    }
 
 
 
@@ -82,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                         {
                             addAgence();
                             getListeAgence();
+                            addUser();
                         }else
                         {
                             List<Agence> listAgence = queryDocumentSnapshots.toObjects(Agence.class);
